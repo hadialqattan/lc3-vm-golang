b8367b34b0af6ebda72a65d18380672c7eca4cbf
Hadi Alqattan <alqattanhadizaki@gmail.com>
2020-10-24 00:40:11 +0300

    Update: memory R/W

--- vm/memory.go
+++ vm/memory.go
@@ -1,41 +1,19 @@
 package vm
 
-import (
-	"github.com/pkg/term"
-)
-
 const (
 	// KBSR is a keyboard status
-	KBSR = 0xFE00
+	KBSR uint16 = 0xFE00
 	// KBDR is a keyboard data
-	KBDR = 0xFE02
+	KBDR uint16 = 0xFE02
 )
 
-func (cpu *CPU) memWrite(address uint16, val uint16) {
+func (cpu *CPU) memoryWrite(address uint16, val uint16) {
 	cpu.Memory[address] = val
 }
 
-func (cpu *CPU) memRead(address uint16) uint16 {
-	if address == KBSR {
-		if checkKey() {
-			cpu.Memory[KBSR] = (1 << 15)
-			cpu.Memory[KBDR] = getchar()
-		} else {
-			cpu.Memory[KBSR] = 0
-		}
+func (cpu *CPU) memoryRead(address uint16) uint16 {
+	if address == KBDR {
+		cpu.memoryWrite(KBSR, cpu.memoryRead(KBSR)&0x7FFF)
 	}
-	return cpu.Memory[address]
-}
-
-func getchar() []byte {
-	t, _ := term.Open("/dev/tty")
-	term.RawMode(t)
-	bytes := make([]byte, 3)
-	numRead, err := t.Read(bytes)
-	t.Restore()
-	t.Close()
-	if err != nil {
-		return nil
-	}
-	return bytes[0:numRead]
+	return uint16(cpu.Memory[address])
 }
