e4f247787dc680ad13659b21c6fef96e98d16182
Hadi Alqattan <alqattanhadizaki@gmail.com>
2020-10-25 19:19:22 +0300

    fromat

--- programs/source/hello_world.asm
+++ programs/source/hello_world.asm
@@ -9,7 +9,7 @@ LEA R0, hello    ; R0 = &hello
 TRAP x22         ; PUTS (print char array at addr in R0)
 hello .STRINGZ "\n-- HELLO WORLD! --\n\n"
 
-LEA	R0,	anykey   ; R0 = &anykey
+LEA R0, anykey   ; R0 = &anykey
 TRAP x22         ; PUTS (print char array at addr in R0)
 TRAP x20         ; GETC (wait for any key to be pressed)
 anykey .STRINGZ	"press any key to exit..."
